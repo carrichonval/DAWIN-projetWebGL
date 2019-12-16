db052c599230d39839aae8d045d032531d721519
valentincarrichon <valentincarrichon@outlook.com>
2019-12-16 23:26:27 +0100

    Ajout 1er personnage, agrandissement zone camera et sol, texture sol provisoire

--- js/script.js
+++ js/script.js
@@ -22,8 +22,6 @@ const Scene = {
         requestAnimationFrame(Scene.animate);
         Scene.vars.raycaster.setFromCamera(Scene.vars.mouse, Scene.vars.camera);
 
-
-
         if (Scene.vars.goldGroup !== undefined) {
             let intersects = Scene.vars.raycaster.intersectObjects(Scene.vars.goldGroup.children, true);
 
@@ -33,7 +31,6 @@ const Scene = {
                 Scene.vars.animSpeed = -0.05;
             }
 
-
         }
 
         Scene.render();
@@ -42,12 +39,45 @@ const Scene = {
         Scene.vars.renderer.render(Scene.vars.scene, Scene.vars.camera);
         Scene.vars.stats.update();
     },
-
     onWindowResize: () => {
         let vars = Scene.vars;
         vars.camera.aspect = window.innerWidth / window.innerHeight;
         vars.camera.updateProjectionMatrix();
         vars.renderer.setSize(window.innerWidth, window.innerHeight);
+    },
+    loadFBX: (file, scale, position, rotation, namespace, callback) => {
+        let vars = Scene.vars;
+        let loader = new FBXLoader();
+
+        if (file === undefined) {
+            return;
+        }
+
+        loader.load('./fbx/' + file, (object) => {
+
+            object.traverse((child) => {
+                if (child.isMesh) {
+
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+
+                }
+            });
+
+            object.position.x = position[0];
+            object.position.y = position[1];
+            object.position.z = position[2];
+
+            object.rotation.x = rotation[0];
+            object.rotation.y = rotation[1];
+            object.rotation.z = rotation[2];
+
+            object.scale.x = object.scale.y = object.scale.z = scale;
+            Scene.vars[namespace] = object;
+
+            callback();
+        });
+
     },
     init: () => {
         let vars = Scene.vars;
@@ -60,7 +90,7 @@ const Scene = {
         // ajout de la scène
         vars.scene = new THREE.Scene();
         vars.scene.background = new THREE.Color(0xa0a0a0);
-        vars.scene.fog = new THREE.Fog(vars.scene.background, 1000, 10000);
+        vars.scene.fog = new THREE.Fog(vars.scene.background, 20000, 20000);
 
         // paramétrage du moteur de rendu
         vars.renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -73,7 +103,7 @@ const Scene = {
         vars.container.appendChild(vars.renderer.domElement);
 
         // ajout de la caméra
-        vars.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 8000);
+        vars.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 30000);
         vars.camera.position.set(2000, 1000, 2000);
 
         // ajout de la lumière
@@ -129,9 +159,14 @@ const Scene = {
 
         // ajout du sol
         let mesh = new THREE.Mesh(
-            new THREE.PlaneBufferGeometry(2000, 2000),
+            new THREE.PlaneBufferGeometry(6000, 6000),
             new THREE.MeshLambertMaterial({ color: new THREE.Color(0x888888) })
         );
+        mesh.traverse((child) => {
+            child.material = new THREE.MeshBasicMaterial({
+                map: new THREE.TextureLoader().load('./texture/marbre.jpg')
+            });
+        });
         mesh.rotation.x = -Math.PI / 2;
         mesh.receiveShadow = false;
         vars.scene.add(mesh);
@@ -147,13 +182,47 @@ const Scene = {
         vars.scene.add(shadowPlane);
 
 
-        vars.texture = new THREE.TextureLoader().load('./texture/marbre.jpg');
-
         let hash = document.location.hash.substr(1);
         if (hash.length !== 0) {
             let text = hash.substring();
             Scene.vars.text = decodeURI(text);
         }
+
+        /*
+                var loader = new FBXLoader();
+                var textureLoader = new THREE.TextureLoader();
+                var map = textureLoader.load('./texture/f_med_scarecrow_backpack.png');
+                var map1 = textureLoader.load('./texture/f_med_scarecrow_head.png');
+                var map2 = textureLoader.load('./texture/f_med_scarecrow.png');
+                var materials = [new THREE.MeshPhongMaterial({ map: map }),
+                    new THREE.MeshPhongMaterial({ map: map1 }),
+                    new THREE.MeshPhongMaterial({ map: map2 })
+                ];
+
+                
+                        loader.load('./fbx/chala.fbx', function(object3d) {
+                            // For any meshes in the model, add our material.
+                            object3d.traverse(function(node) {
+
+                                if (node.isMesh) node.material = materials;
+
+                            });
+                            object3d.position.set(0, 0, 0);
+                            object3d.scale.set(500, 500, 500)
+                            console.log(object3d)
+                            vars.scene.add(object3d);
+                        });*/
+
+
+
+        Scene.loadFBX("PersonnagePaille.fbx", 500, [0, 0, 0], [0, 0, 0], 'PersonnagePaille', () => {
+            let vars = Scene.vars;
+            let PersonnagePaille = new THREE.Group();
+            PersonnagePaille.add(vars.PersonnagePaille);
+            PersonnagePaille.position.set(0, 0, 0);
+            PersonnagePaille.rotation.y = Math.PI;
+            vars.scene.add(PersonnagePaille);
+        });
 
 
         // ajout des controles
