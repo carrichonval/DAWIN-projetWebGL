00d5b8543aa756754d610b319d802b52242cff33
vcarrichon <v.carrichon@selectup.com>
2019-12-19 16:04:07 +0100

    Réorganisation du code pour plus de lisibilité, changement background color, changement méthode importation des 2 modeles FBX

--- js/script.js
+++ js/script.js
@@ -17,6 +17,9 @@ const Scene = {
         raycaster: new THREE.Raycaster(),
         animSpeed: null,
         animPercent: 0.00,
+        loaderFBX: new FBXLoader(),
+        loaderGLTF:new GLTFLoader(),
+        mixer:null
     },
     animate: () => {
         requestAnimationFrame(Scene.animate);
@@ -40,75 +43,104 @@ const Scene = {
         Scene.vars.stats.update();
     },
     onWindowResize: () => {
-        let vars = Scene.vars;
-        vars.camera.aspect = window.innerWidth / window.innerHeight;
-        vars.camera.updateProjectionMatrix();
-        vars.renderer.setSize(window.innerWidth, window.innerHeight);
+        Scene.vars.camera.aspect = window.innerWidth / window.innerHeight;
+        Scene.vars.camera.updateProjectionMatrix();
+        Scene.vars.renderer.setSize(window.innerWidth, window.innerHeight);
     },
     onMouseMove: (event) => {
         Scene.vars.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
         Scene.vars.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
     },
-    loadFBX: (file, scale, position, rotation, namespace, callback) => {
+    loadObject:()=>{
         let vars = Scene.vars;
-        let loader = new FBXLoader();
 
-        if (file === undefined) {
-            return;
-        }
+        /*fbxloader.load('./fbx/Letter/A.fbx', function(model) {
+            model.position.set(0, 0, 1500);
+            model.scale.set(3, 3, 3);
+            //console.log(gltf)
+            vars.scene.add(model);
+        });*/
 
-        loader.load('./fbx/' + file, (object) => {
-
-            object.traverse((child) => {
-                if (child.isMesh) {
-
-                    child.castShadow = true;
-                    child.receiveShadow = true;
-
-                }
-            });
-
-            object.position.x = position[0];
-            object.position.y = position[1];
-            object.position.z = position[2];
-
-            object.rotation.x = rotation[0];
-            object.rotation.y = rotation[1];
-            object.rotation.z = rotation[2];
-
-            object.scale.x = object.scale.y = object.scale.z = scale;
-            Scene.vars[namespace] = object;
-
-            callback();
+        vars.loaderGLTF.load('./fbx/Zelda/scene.gltf', function(gltf) {
+            const model = gltf.scene;
+            model.position.set(-1300, -10, 0);
+            model.scale.set(3, 3, 3);
+            model.name = "Zelda";
+            vars[model.name] = gltf;
+            vars.scene.add(model);
         });
 
+        vars.loaderGLTF.load('./fbx/Junkrat/scene.gltf', function(gltf) {
+            const model = gltf.scene;
+            model.position.set(-700, 0, 600);
+            model.name = "Junkrat";
+            vars[model.name] = gltf;
+            vars.scene.add(model);
+        });
+
+        vars.loaderGLTF.load('./fbx/zebrus/scene.gltf', function(gltf) {
+            const model = gltf.scene;
+            model.position.set(0, 1000, 0);
+            model.scale.set(250, 250, 250);
+            model.name = "Zebrus";
+            vars[model.name] = gltf;
+            vars.scene.add(model);
+        });
+
+        vars.loaderGLTF.load('./fbx/BlackDragon/scene.gltf', function(gltf) {
+            const model = gltf.scene;
+            model.scale.set(0.5, 0.5, 0.5);
+            model.name = "BlackDragon";
+            vars[model.name] = gltf;
+            vars.scene.add(model);
+        });
+
+        vars.loaderGLTF.load('./fbx/Crab/scene.gltf', function(gltf) {
+            const model = gltf.scene;
+            model.position.set(0, 200, 1000);
+            model.rotation.y = Math.PI;
+            model.scale.set(50, 50, 50);
+            model.name = "Crab";
+            vars[model.name] = gltf;
+            vars.scene.add(model);
+        });
+
+        vars.loaderGLTF.load('./fbx/Phoenix/scene.gltf', function(gltf) {
+            const model = gltf.scene;
+            model.position.set(800, 450, 200);
+            model.rotation.y = -Math.PI / 2;
+            model.scale.set(1, 1, 1);
+            model.name = "Phoenix";
+            vars[model.name] = gltf;
+            vars.scene.add(model);
+        });
+
+        /* vars.loaderGLTF.load('./fbx/LetterA.glb', function(gltf) {
+             const model = gltf.scene;
+             model.position.set(-300, 200, 1500);
+             model.scale.set(1, 1, 1);
+             Scene.vars[model.name] = gltf;
+             //console.log(gltf)
+             vars.scene.add(model);
+         });*/
+
+          vars.loaderFBX.load('./fbx/PersonnagePaille.fbx', function(object) {
+            object.position.set(1100, 0, 400);
+            object.scale.set(500,500,500);
+            object.rotation.y=Math.PI;
+            object.name="PersonnagePaille";
+            vars.scene.add(object);
+         });
+
+         vars.loaderFBX.load('./fbx/Finger.fbx', function(object) {
+            object.position.set(700, 0, 600);
+            object.scale.set(2,2,2);
+            object.name="Finger";
+            vars.scene.add(object);
+         });
     },
-    init: () => {
+    addLights:()=>{
         let vars = Scene.vars;
-
-        // Préparer le container pour la scène
-        vars.container = document.createElement('div');
-        vars.container.classList.add('fullscreen');
-        document.body.appendChild(vars.container);
-
-        // ajout de la scène
-        vars.scene = new THREE.Scene();
-        vars.scene.background = new THREE.Color(0xa0a0a0);
-        vars.scene.fog = new THREE.Fog(vars.scene.background, 100000, 100000);
-
-        // paramétrage du moteur de rendu
-        vars.renderer = new THREE.WebGLRenderer({ antialias: true });
-        vars.renderer.setPixelRatio(window.devicePixelRatio);
-        vars.renderer.setSize(window.innerWidth, window.innerHeight);
-
-        vars.renderer.shadowMap.enabled = true;
-        vars.renderer.shadowMapSoft = true;
-
-        vars.container.appendChild(vars.renderer.domElement);
-
-        // ajout de la caméra
-        vars.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 30000);
-        vars.camera.position.set(0, 1400, 4000);
 
         // ajout de la lumière
         const lightIntensityHemisphere = .5;
@@ -160,137 +192,62 @@ const Scene = {
         vars.scene.add(light3);
         // let helper3 = new THREE.DirectionalLightHelper(light3, 5);
         // vars.scene.add(helper3);
-
-
-
-
-
-        let mixer;
-
-        const loader = new GLTFLoader();
-        const fbxloader = new FBXLoader();
-
-        /*fbxloader.load('./fbx/Letter/A.fbx', function(model) {
-            model.position.set(0, 0, 1500);
-            model.scale.set(3, 3, 3);
-            //console.log(gltf)
-            vars.scene.add(model);
-        });*/
-
-
-
-        loader.load('./fbx/Zelda/scene.gltf', function(gltf) {
-            const model = gltf.scene;
-            model.position.set(-1300, -10, 0);
-            model.scale.set(3, 3, 3);
-            model.name = "Zelda";
-            Scene.vars[model.name] = gltf;
-            //console.log(gltf)
-            vars.scene.add(model);
-        });
-
-        loader.load('./fbx/Junkrat/scene.gltf', function(gltf) {
-            const model = gltf.scene;
-            model.position.set(-700, 0, 600);
-            model.name = "Junkrat";
-            Scene.vars[model.name] = gltf;
-            //console.log(gltf)
-            vars.scene.add(model);
-        });
-
-        loader.load('./fbx/zebrus/scene.gltf', function(gltf) {
-            const model = gltf.scene;
-            model.position.set(0, 1000, 0);
-            model.scale.set(250, 250, 250);
-            model.name = "Zebrus";
-            Scene.vars[model.name] = gltf;
-            //console.log(gltf)
-            vars.scene.add(model);
-        });
-
-        loader.load('./fbx/BlackDragon/scene.gltf', function(gltf) {
-            const model = gltf.scene;
-            model.scale.set(0.5, 0.5, 0.5);
-            model.name = "BlackDragon";
-            Scene.vars[model.name] = gltf;
-            //console.log(gltf)
-            vars.scene.add(model);
-        });
-
-        loader.load('./fbx/Crab/scene.gltf', function(gltf) {
-            const model = gltf.scene;
-            model.position.set(0, 200, 1000);
-            model.rotation.y = Math.PI;
-            model.scale.set(50, 50, 50);
-            model.name = "Crab";
-            Scene.vars[model.name] = gltf;
-            //console.log(gltf)
-            vars.scene.add(model);
-        });
-
-
-        loader.load('./fbx/Phoenix/scene.gltf', function(gltf) {
-            const model = gltf.scene;
-            model.position.set(800, 450, 200);
-            model.rotation.y = -Math.PI / 2;
-            model.scale.set(1, 1, 1);
-            model.name = "Phoenix";
-            Scene.vars[model.name] = gltf;
-            //console.log(gltf)
-            vars.scene.add(model);
-        });
-
-        /* loader.load('./fbx/LetterA.glb', function(gltf) {
-             const model = gltf.scene;
-             model.position.set(-300, 200, 1500);
-             model.scale.set(1, 1, 1);
-             Scene.vars[model.name] = gltf;
-             //console.log(gltf)
-             vars.scene.add(model);
-         });*/
-
-
-        Scene.loadFBX("PersonnagePaille.fbx", 500, [0, 0, 0], [0, 0, 0], 'PersonnagePaille', () => {
-            Scene.loadFBX("Finger.fbx", 2, [0, 0, 0], [0, 0, 0], 'Finger', () => {
-
-                let vars = Scene.vars;
-
-                let PersonnagePaille = new THREE.Group();
-                PersonnagePaille.add(vars.PersonnagePaille);
-                PersonnagePaille.position.set(1100, 0, 400);
-                PersonnagePaille.rotation.y = Math.PI;
-                PersonnagePaille.name = "PersonnagePaille"
-                    //console.log(PersonnagePaille)
-                vars.scene.add(PersonnagePaille);
-
-                let Finger = new THREE.Group();
-                Finger.add(vars.Finger);
-                Finger.position.set(700, 0, 600);
-                Finger.name = "Finger";
-                //console.log(Finger)
-                vars.scene.add(Finger);
-
-            });
-        });
-        console.log(Scene.vars)
-
-
+    },
+    addControls:()=>{
         // ajout des controles
-        vars.controls = new OrbitControls(vars.camera, vars.renderer.domElement);
-        // vars.controls.minDistance = 300;
+        Scene.vars.controls = new OrbitControls(Scene.vars.camera, Scene.vars.renderer.domElement);
+        //vars.controls.minDistance = 300;
         //vars.controls.maxDistance = 600;
-        //vars.controls.minPolarAngle = Math.PI / 4;
-        //vars.controls.maxPolarAngle = Math.PI / 2;
-        //vars.controls.minAzimuthAngle = -Math.PI / 4;
-        //vars.controls.maxAzimuthAngle = Math.PI / 4;
-        vars.controls.target.set(0, 100, 0);
-        vars.controls.update();
+        //vars.controls.minPolarAngle = Math.PI / 2;
+        Scene.vars.controls.maxPolarAngle = Math.PI / 2;
+        Scene.vars.controls.minAzimuthAngle = -Math.PI / 2;
+        Scene.vars.controls.maxAzimuthAngle = Math.PI / 2;
+        Scene.vars.controls.target.set(0, 100, 0);
+        Scene.vars.controls.update();
+    },
+    addScene:()=>{
+        // Préparer le container pour la scène
+        Scene.vars.container = document.createElement('div');
+        Scene.vars.container.classList.add('fullscreen');
+        document.body.appendChild(Scene.vars.container);
+
+        // ajout de la scène
+        Scene.vars.scene = new THREE.Scene();
+        Scene.vars.scene.background = new THREE.Color(0xad1818);
+    },
+    moteurRendu:()=>{
+        // paramétrage du moteur de rendu
+        Scene.vars.renderer = new THREE.WebGLRenderer({ antialias: true });
+        Scene.vars.renderer.setPixelRatio(window.devicePixelRatio);
+        Scene.vars.renderer.setSize(window.innerWidth, window.innerHeight);
+
+        Scene.vars.renderer.shadowMap.enabled = true;
+        Scene.vars.renderer.shadowMapSoft = true;
+
+        Scene.vars.container.appendChild(Scene.vars.renderer.domElement);
+    },
+    addCamera:()=>{
+        // ajout de la caméra
+        Scene.vars.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 30000);
+        Scene.vars.camera.position.set(0, 1400, 4000);
+    },
+    addStats:()=>{
+        Scene.vars.stats = new Stats();
+        Scene.vars.container.appendChild(Scene.vars.stats.dom);
+    },
+    init: () => {
+        Scene.addScene();
+        Scene.moteurRendu();
+        Scene.addCamera();
+        Scene.addLights();
+        Scene.addControls();
+        Scene.loadObject();
+        Scene.addStats();
+        
+        console.log(Scene.vars)
 
         window.addEventListener('resize', Scene.onWindowResize, false);
         window.addEventListener('mousemove', Scene.onMouseMove, false);
-
-        vars.stats = new Stats();
-        vars.container.appendChild(vars.stats.dom);
 
         Scene.animate();
     }
